d6ee1ea3789bf19078f75ef181c79451df45ddd9
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2024-01-26 13:45:32 +0530

    usercreation

--- bookstore/webapp/views.py
+++ bookstore/webapp/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import CreateUserForm, LoginForm
 
 
 def home(request):
     # return HttpResponse("hello world!")
     return render(request, "webapp/index.html")
+
+
+# register
+
+
+def register(request):
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # return redirect("")
+    context = {"form": form}
+    return render(request, "webapp/register.html", context=context)
