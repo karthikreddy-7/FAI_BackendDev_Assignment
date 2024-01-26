7eff6189742686c9513fbe17b55ecf6b79f88333
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2024-01-26 14:13:09 +0530

    login

--- bookstore/webapp/views.py
+++ bookstore/webapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import CreateUserForm, LoginForm
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
 
 
 def home(request):
@@ -20,3 +22,21 @@ def register(request):
             # return redirect("")
     context = {"form": form}
     return render(request, "webapp/register.html", context=context)
+
+
+# login a user
+
+
+def my_login(request):
+    form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                # return redirect("")
+    context = {"form": form}
+    return render(request, "webapp/my-login.html", context=context)
